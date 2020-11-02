8a82e3e21971baeeb896be824297ee59092ae9b8
bjoernkneisel <info@bjoern-kneisel.de>
2020-11-02 19:26:32 +0100

    Fixed navbar styling for mobile devices

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -58,12 +58,17 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@media only screen and (max-width: 768px) {
+  .navbar-item {
+    color: #242423!important;
+  }
+}
 .navbar {
   background-color: #242423;
 }
 .navbar-brand {
   .navbar-item {
-    color: #fff!important;
+    color: #fff!important
   }
 }
 .navbar-item {
