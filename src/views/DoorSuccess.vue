0900e590f6d2715b0719bb084178776ef9249546
bjoernkneisel <info@bjoern-kneisel.de>
2020-11-20 13:15:37 +0100

    Added success code to redeem correct solution

--- src/views/DoorSuccess.vue
+++ src/views/DoorSuccess.vue
@@ -3,8 +3,9 @@
     <div class="hero-body">
       <div class="container">
         <h1 class="title">Du hast erfolgreich Türchen Nr. {{this.$route.params.id}} geöffnet!</h1>
-        <p class="is-size-5">Schicke Björn hiervon einen Screenshot für deine Belohnung!</p>
-        <p class="is-size-5">Hinweis: {{hinweis}}</p> 
+        <p class="is-size-5">Schicke Björn folgenden Lösungs-Code für deine Belohnung: <strong>{{lösungsCode}}</strong>!</p>
+        <br>
+        <router-link to="/Calendar" class="button is-primary">Zurück</router-link>
       </div>
     </div>
   </section>
@@ -14,8 +15,8 @@
 export default {
   name: 'DoorSuccess',
   computed: {
-    hinweis() {
-      return this.$cards[this.$route.params.id].surprise;
+    lösungsCode() {
+      return this.$cards[this.$route.params.id].lösungsCode;
     }
   }
 }
